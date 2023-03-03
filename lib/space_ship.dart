3582eeccbf0c5dd5088890b4421a44bceedf77f3
kangminchan99 <114412280+kangminchan99@users.noreply.github.com>
2023-03-03 21:29:54 +0900

    Update space_ship.dart

--- lib/space_ship.dart
+++ lib/space_ship.dart
@@ -1,37 +1,32 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
-import 'package:flutter_game/joystick.dart';
-import 'package:flutter_game/ship.dart';
-
 import 'engine.dart';
+import 'joystick.dart';
+import 'ship.dart';
 
 class SpaceShip extends StatelessWidget {
   SpaceShip({Key? key}) : super(key: key) {
-    _joystick = JoyStick(onMove: (int direction) => {_ship.move(direction)});
+    _joystick = Joystick(onMove: (int direction) => {_ship.move(direction)});
 
     _ship = Ship();
 
     _gameEngine.getControls().addControl(_joystick);
     _gameEngine.getControls().addControl(_ship);
+    _gameEngine.start();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('GAME')),
-      body: SizedBox(
-        // 화면 사이즈 맥스로 설정
-        width: double.infinity,
-        height: double.infinity,
-        child: _gameEngine.getCustomPaint(),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text("GameEngine Demo"),
+        ),
+        body: SizedBox(
+            width: double.infinity,
+            height: double.infinity,
+            child: _gameEngine.getCustomPaint()));
   }
 
   final _gameEngine = GameEngine();
-
-  // 참조변수 선언
   late final _joystick;
   late final _ship;
 }
