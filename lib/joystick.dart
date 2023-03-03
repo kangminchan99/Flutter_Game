b9a2d53da3ebe11390c199b9e753d9bc5ea921d9
kangminchan99 <114412280+kangminchan99@users.noreply.github.com>
2023-03-03 21:28:44 +0900

    Update joystick.dart

--- lib/joystick.dart
+++ lib/joystick.dart
@@ -1,38 +1,39 @@
 import 'package:flutter/material.dart';
-import 'package:get/route_manager.dart';
+import 'engine.dart';
 
-// 정수의 디렉션 파라미터를 가지는 함수의 형태
-typedef MoveCallback = void Function(int direction);
-
-const BUTTON_SIZE = 60;
-const SHIP_SIZE = 60;
+const BUTTON_SIZE = 60.0;
 const BUTTON_POSITION_LEFT = -1;
 const BUTTON_POSITION_RIGHT = 1;
 
-class JoyStick extends GetMaterialController {
+typedef MoveCallback = void Function(int direction);
+
+class Joystick extends GameControl {
   final MoveCallback onMove;
 
-  // 객체가 생성될 때 무조건 이벤트를 지정 받도록 한다.
-  JoyStick({required this.onMove});
+  Joystick({required this.onMove});
 
   @override
-  void Start(Canvas canvas, Size size, int current) {
-    var x = 20;
-    var y = size.height - BUTTON_SIZE;
+  void onStart(Canvas canvas, Size size, int current) {
+    x = 20;
+    y = size.height - BUTTON_SIZE * 2;
+    width = BUTTON_SIZE;
+    height = BUTTON_SIZE;
+    paint.color = Colors.red.withOpacity(0.1);
   }
 
   @override
   void tick(Canvas canvas, Size size, int current, int term) {
     const radius = BUTTON_SIZE / 2;
-    var width = SHIP_SIZE;
-    var height = SHIP_SIZE;
-    var x = (size.width - width) / 2;
-    var y = size.height - SHIP_SIZE * 2;
-    canvas.drawCircle(Offset(x + radius, y + radius), radius, Paint());
+    canvas.drawCircle(Offset(x + radius, y + radius), radius, paint);
   }
 
   @override
   void onHorizontalDragStart(DragStartDetails details) {
     onMove(BUTTON_POSITION_LEFT);
+  }
+
+  @override
+  void onHorizontalDragEnd(DragEndDetails details) {
+    onMove(0);
   }
 }
